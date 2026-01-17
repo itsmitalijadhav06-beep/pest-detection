3234514b17f4af9087afa7f2756dfc8c03600e3c
Mitali <its.mitalijadhav06@gmail.com>
2026-01-17 22:55:42 +0530

    db.py

--- config/db.py
+++ config/db.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://itsmitalijadhav06_db_user:WPM6j9eQdMXCarZA@pest-detection.madzvbj.mongodb.net/?appName=pest-detection")
+MONGO_URI = os.getenv("MONGO_URI")
 
+if not MONGO_URI:
+    raise RuntimeError("MONGO_URI environment variable is not set")
 
 client = MongoClient(MONGO_URI)
 
