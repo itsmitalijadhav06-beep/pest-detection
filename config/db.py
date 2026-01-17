a5876053ea2f19a41c70a985f44ce8a2e60fa22b
Mitali <its.mitalijadhav06@gmail.com>
2026-01-17 22:48:16 +0530

    to db.py

--- config/db.py
+++ config/db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://127.0.0.1:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://itsmitalijadhav06_db_user:WPM6j9eQdMXCarZA@pest-detection.madzvbj.mongodb.net/?appName=pest-detection")
+
 
 client = MongoClient(MONGO_URI)
 
